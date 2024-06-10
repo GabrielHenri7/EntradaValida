26314bcf649ab0bf001737150b82882debf8f761
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-10 20:07:03 -0300

    Atividade Concluída

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("--->> ATIVIDADE ENTRADA VÁLIDA <<---");
+ 
+int resolução;
+do 
+{
+ 
+    Console.Write("\nDigite um número: ");
+    resolução = Convert.ToInt32(Console.ReadLine())!;
+ 
+}
+while(resolução < 0 || resolução >= 10);
+ 
+if (resolução== 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("\nOperação cancelada");
+    Console.ResetColor();
+}
+ 
+else
+{
+    Console.WriteLine($"\nNúmero escolhido = {resolução}");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("\nVolte logo!");
+    Console.ResetColor();
+}
